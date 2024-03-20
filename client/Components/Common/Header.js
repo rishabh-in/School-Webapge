8dd374501ea1b5be9c3be41d68ae114cbf69410b
Rishabh Tiwari <13rishabhtiwari@gmail.com>
2024-03-20 16:09:56 +0530

    Integrated apis for fetching header and body info

--- client/Components/Common/Header.js
+++ client/Components/Common/Header.js
@@ -1,21 +1,38 @@
+import { STRIPE_BASE_URL } from '../../Constant/constant';
 import '../../styles/header.css';
-import logo from '../../public/static/logo.png'
+import { useEffect, useState } from 'react';
+import Loading from './Loading';
 
 const Header = () => {
+  const [headerData, setHeaderData] = useState();
+
+  useEffect(() => {
+    const fetchHeaderData = async() => {
+      const response = await fetch("http://localhost:1337/api/home-static?populate=*");
+      const json = await response.json();
+      setHeaderData(json.data);
+    } 
+    fetchHeaderData()
+  }, []);
+
+  if(headerData == null) {
+    return <Loading />
+  }
+  const headerLogoDetails = headerData?.attributes?.headerLogo?.data[0];
   return (
     <div className='header-container-wrapper'>
       <div className="header-container">
         <div className="logo-container">
-          <img src={logo}  alt='logo-png'/>
+          <img src={STRIPE_BASE_URL + headerLogoDetails.attributes.url}  alt='logo-png'/>
         </div>
         <div className="menu-items-container">
           <ul>
-            <li>The School</li>
-            <li>Academics</li>
-            <li>Life @ DBTR</li>
-            <li>Contact us</li>
-            <button className='btn-csr'>csr</button>
-            <button className='btn-donate'>Donate <span className="heart">♥️</span></button>
+            <li>{headerData.attributes.menuItem1}</li>
+            <li>{headerData.attributes.menuItem2}</li>
+            <li>{headerData.attributes.menuItem3}</li>
+            <li>{headerData.attributes.menuItem4}</li>
+            <button className='btn-csr'>{headerData.attributes.menuBtn1}</button>
+            <button className='btn-donate'>{headerData.attributes.menuBtn2} <span className="heart">♥️</span></button>
           </ul>
           
         </div>
--- client/Components/Gallery/PageInfo.js
+++ client/Components/Gallery/PageInfo.js
@@ -1,8 +1,35 @@
-import "../../styles/gallery.css"
+import { useEffect, useState } from "react";
+import "../../styles/gallery.css";
+import Loading from "../Common/Loading";
+import { STRIPE_BASE_URL } from "../../Constant/constant";
 
 const PageInfo = () => {
+
+  const [bodyTopContainerDetails, setBodyTopContainerDetails] = useState();
+
+  useEffect(() => {
+    const fetchToBodyContent = async() => {
+      const response = await fetch("http://localhost:1337/api/gallery-body?populate=*")
+      const json = await response.json();
+      setBodyTopContainerDetails(json.data);
+
+    }
+    fetchToBodyContent();
+  }, [])
+
+
+
+  if(!bodyTopContainerDetails) {
+    return <Loading />
+  }
+
+  const IMG_URL = `${STRIPE_BASE_URL}${bodyTopContainerDetails?.attributes?.background?.data[0].attributes?.url}`
+  console.log("img", IMG_URL) 
   return (
     <div className="info-container-wrapper">
+      <div className="background-image">
+        <img src={IMG_URL} />
+      </div>
       <div className="info-container">
         <h1>Our events gallery</h1>
         <p>Events at DBTR are filled with joyous occasions, cultural gatherings, and learning opportunities that bring us all together.</p>
