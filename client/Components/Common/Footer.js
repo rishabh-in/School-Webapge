aabf02e1454b7dcbb7ddc8956f7643241684be10
Rishabh Tiwari <13rishabhtiwari@gmail.com>
2024-03-21 14:19:39 +0530

    footer api integration and styling

--- client/Components/Common/Footer.js
+++ client/Components/Common/Footer.js
@@ -1,50 +1,118 @@
 import "../../styles/footer.css"
 import logo from "../../public/static/logo.png"
 import {Link} from 'react-router-dom'
+import { useEffect, useState } from "react"
+import { FETCH_FOOTER_DATA, STRIPE_BASE_URL } from "../../Constant/constant"
+import Loading from "./Loading"
 
 const Footer = () => {
+  const [footerData, setFooterData] = useState();
 
+  useEffect(() => {
+    const fetchFooterData = async() => {
+      const response = await fetch(FETCH_FOOTER_DATA);
+      const json = await response.json();
+      setFooterData(json.data);
+    }
+    fetchFooterData();
+  }, [])
+  
+  if(!footerData) {
+    return <Loading />
+  }
+
+  console.log(footerData);
+  const {
+    schoolName,
+    schoolMotto,
+    schoolEst,
+    quickLinks,
+    quickLink1,
+    quickLink2,
+    quickLink3,
+    quickLink4,
+    contact,
+    address,
+    number,
+    email,
+    text1,
+    btnText,
+    credits,
+    sitemap,
+    designedByText,
+  } = footerData?.attributes;
+
+  const footerLogo = footerData?.attributes.footerLogo?.data[0];
+  const twitterIcon = footerData?.attributes.twitter?.data[0];
+  const linkedInIcon = footerData?.attributes.linkedIn?.data[0];
+  const instaIcon = footerData?.attributes.insta?.data[0];
+  const facebookIcon = footerData?.attributes.facebook?.data[0];
+  const youtubeIcon = footerData?.attributes.youtube?.data[0];
+  const pepperContentLogo = footerData?.attributes.pepperContentLogo?.data[0];
+  console.log("twitter", twitterIcon)
   return (
       <div className="parent-footer-wrapper">
         <div className="footer-wrapper">
           <div className="footer-top">
-            <div className="footer-logo-container">
-              <img className="footer-logo-img" src={logo} alt="Footer Logo" />
-            </div>
-              <div className="school-details">
-                <p className="school-name">DBTR National Higher Secondary School</p>
-                <p className="school-motto">Virtuousness is Life</p>
-                <p className="school-est">Established in 1901, DBTR is situated in the temple town of Mayiladuthurai.</p>
+            <div className="footer-logo-wrapper">
+              <div className="footer-logo-container">
+                <img className="footer-logo-img" src={logo} alt="Footer Logo" />
               </div>
+              <div className="school-details">
+                  <p className="school-name">{schoolName}</p>
+                  <p className="school-motto">{schoolMotto}</p>
+                  <p className="school-est">{schoolEst}</p>
+                </div>
+            </div>
               <div className="quick-links">
-                <p className="section-title">QUICK LINKS</p>
+                <p className="section-title">{quickLinks}</p>
                 <ul>
-                  <li><Link to="/admission">Admissions</Link></li>
-                  <li><Link to="/alumni">Alumni association</Link></li>
-                  <li><Link to="donate">Donate</Link></li>
-                  <li><Link to="events">Events</Link></li>
+                  <li><Link to="/admission">{quickLink1}</Link></li>
+                  <li><Link to="/alumni">{quickLink2}</Link></li>
+                  <li><Link to="donate">{quickLink3}</Link></li>
+                  <li><Link to="events">{quickLink4}</Link></li>
                 </ul>
               </div>
               <div className="contact-info">
-                <p className="section-title">CONTACT</p>
-                <p>DBTR NHSS, Mahadhana Street, Kamarajar Salai, Mayiladuthurai, Tamilnadu – 609001</p>
-                <p>+91.436.422.3272</p>
-                <p>contact@nationalhighschool.in</p>
+                <p className="section-title">{contact}</p>
+                <p>{address}</p>
+                <p className="contact-phone">{number}</p>
+                <p>{email}</p>
               </div>
               <div className="donate-section">
-                <p>Big or small, you can make an impact.</p>
-                <button className="btn-donate">Donate <span className="heart">♥️</span></button>
+                <p>{text1}</p>
+                <button className="footer-btn-donate"><Link>{btnText} <span className="heart">♥️</span></Link></button>
               </div>
           </div>
           <div className="footer-bottom">
+            <div className="border-line"></div>
             <div className="footer-bottom-first-half">
+              <div>
+                <p>{credits}</p>
+                <div>
+                  <div></div>
+                </div>
+                <p>{sitemap}</p>
+              </div>
+              <div className="social-media-icons">
+                <div>
+                  <Link to="/"><img src={STRIPE_BASE_URL + twitterIcon.attributes.url} /></Link>
+                  <Link to="/"><img src={STRIPE_BASE_URL + linkedInIcon.attributes.url} /></Link>
+                  <Link to="/"><img src={STRIPE_BASE_URL + instaIcon.attributes.url} /></Link>
+                  <Link to="/"><img src={STRIPE_BASE_URL + facebookIcon.attributes.url} /></Link>
+                  <Link to="/"><img src={STRIPE_BASE_URL + youtubeIcon.attributes.url} /></Link>
+                </div>
+              </div>
             </div>
             <div className="credit">
+              <div>
+                <p>{designedByText}</p>
+                <Link to="https://www.peppersquare.com/" target="_blank"><img src={STRIPE_BASE_URL + pepperContentLogo.attributes.url} /></Link>
+              </div>
             </div>
           </div>
         </div>
     </div>
   )
 }
-
 export default Footer;
